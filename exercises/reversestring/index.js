c3cda55e16bd9be25a42ce866ac30a155b506005
Jillian Gibson <jillianlgibson@gmail.com>
2021-05-14 13:40:12 -0700

    reverse string challage passed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//Questions to ask:
+    // Are there any conditions for the string that will be passed? No
+    // Will the string contain regular or special characters? No
 
+//Restate the question out loud:
+    // Given a string input of only ASCII characters, I want to create a function that returns a new string with its characters in the reverse order of the input string. I want to return a new string because I want to avoid mutating the original string input.
+  
+// Break down the steps:
+    // 1. convert the string into an array with each character in the string as an element
+        // step 1   let ans = str.split(' ') 
+    // 2. reverse the order of the elements in the array
+        // step 2   ans.reverse()
+    // 3. convert the reversed array back into a string
+        // step 3   ans = ans.join(' ') 
+    // 4. return the reversed string
+        // step 4   return ans
+    // 5. convert to DRY code
+
+function reverse(str) {
+  let ans = str.split('')
+  ans.reverse()
+  ans = ans.join('')
+  return ans
+}
 module.exports = reverse;
