dade3253e7b7e07635dca3c016ae9a6921dddeeb
Jillian Gibson <jillianlgibson@gmail.com>
2021-05-17 13:31:00 -0700

    refactor solution with .reduce for DRY code  - test passes

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -31,19 +31,16 @@
 // }
 // module.exports = reverse;
 
-// VERSION 2 with CONDITIONS:
-    // Are there any conditions for the string that will be passed? Yes - can not use .reverse()
-    // Will the string contain regular or special characters? No
-
-// Break down the steps:
-    // 1. create a variable to serve as the return value and set it as an empty string, call it reversed.
-      // step 1   let reversed = ''
-    // 2. loop through the characters in the input string
-      // step 2   for(let char of string) { }
-    //3.  Within each loop iteration, add the character to the beginning of the reversed string
-      // step 3   reversed = char + reversed;
-    // 4. return reversed
-      // step 4   return reversed;
+// VERSION 2 with CONDITIONS - can not use .reverse():
+  // Break down the steps:
+      // 1. create a variable to serve as the return value and set it as an empty string, call it reversed.
+        // step 1   let reversed = ''
+      // 2. loop through the characters in the input string
+        // step 2   for(let char of string) { }
+      //3.  Within each loop iteration, add the character to the beginning of the reversed string
+        // step 3   reversed = char + reversed;
+      // 4. return reversed
+        // step 4   return reversed;
 
 // function reverse(str) {
 //   let reversed = '' //step 1
@@ -54,23 +51,24 @@
 // }
 // module.exports = reverse;
 
-// VERSION 3 with CONDITIONS:
-// Break down the steps:
-    // 1. convert the string into an array with each character in the string as an element
-        // step 1   str.split(' ') 
-    // 2. use .reduce() to condense the items in the array to one single string value
-        // reduce take two arguments: ((1, 2) => {}, and an empty string '')
-        // step 2   .reduce((reversed, char) => {     
-        //          }, '');
-    // 3. return character and the reversed string
-        // step 3   return char + reversed;
-    // 4. return the result - add return to step 1
-        // step 4   return str.spilt('')
+// VERSION 3 with CONDITIONS - can not use .reverse():
+  // Break down the steps:
+      // 1. convert the string into an array with each character in the string as an element
+          // step 1   str.split(' ') 
+      // 2. use .reduce() to condense the items in the array to one single string value
+          // reduce take two arguments: ((1, 2) => {}, and an empty string '')
+          // step 2   .reduce((reversed, char) => {     
+          //          }, '');
+      // 3. return character and the reversed string
+          // step 3   return char + reversed;
+      // 4. return the result - add return to step 1
+          // step 4   return str.split('')  
+                //      .reduce((reversed, char) => { 
+                //        return char + reversed; 
+                //       }, '');
+      //5. clean up syntax with DRY code
 
 function reverse(str) {
-  return str.split('') //step 1  //step 4
-    .reduce((reversed, char) => { //step 2
-      return char + reversed; //step 3
-    }, '');
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
 module.exports = reverse;
